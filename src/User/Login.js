61815d88f0d4ee5eca0aabaea77fad26e4a23e9b
nrec77 <58744142+nrec77@users.noreply.github.com>
2021-10-05 15:12:37 -0500

    Update Login.js

--- src/User/Login.js
+++ src/User/Login.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Login() {
+export default function Login({ dispatchUser }) {
+
+    const [username, setUsername] = useState('');
+
+    function handleUsername (evt) { setUsername(evt.target.value) } 
+
    return (
-        <form onSubmit={evt => evt.preventDefault()}>
-            <label htmlFor="login-username">Username : </label>
-            <input type="text" name="login-username" id="login-username" />
-
+        <form onSubmit={evt => {evt.preventDefault(); dispatchUser({type:"LOGIN", username})} }>
+            <label htmlFor="login-username">Username:</label>
+            <input type="text" name="login-username" value={username} onChange={handleUsername}  id="login-username" />
+            
             <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-            <label htmlFor="login-password">Password : </label>
+            <label htmlFor="login-password">Password:</label>
             <input type="password" name="login-password" id="login-password" />
 
             <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
