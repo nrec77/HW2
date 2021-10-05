3f7e98d5a1441f23f0eed77a8856612fff805e2a
nrec77 <58744142+nrec77@users.noreply.github.com>
2021-10-05 15:10:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
+import {useState, useReducer, useEffect} from 'react';
 import UserBar from './User/UserBar'
 import CreateToDo from './CreateToDo'
+import ToDoList from './ToDoList'
+import appReducer from './reducers';
 
 function App() {
+
+  const initialToDo = [
+    {
+      title: "ToDo 1",
+      description: "Do Something 1",
+      dateCreated: Date.now(),
+      complete: "false",
+      dateCompleted: ""
+    },
+    {
+      title: "ToDo 2",
+      description: "Do Something 2",
+      dateCreated: Date.now(),
+      complete: "false",
+      dateCompleted: ""
+    }
+  ]
+
+  const [ state, dispatch ] = useReducer(appReducer, { user: '', todos: initialToDo })
+
+  const {user, todos} = state;
+
   return (
       <div>
-        <UserBar />
+        <UserBar user={user} dispatchUser={dispatch}/>
         <br></br><br></br>
-        <CreateToDo user="Paul"/>
+        {user && <CreateToDo user={user} dispatch={dispatch} /> }
+        <ToDoList todos={todos} />
         </div>
   )
-    
-  
+
 }
 export default App;
