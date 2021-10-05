751cf01a1fc17d07c02dcb1b49d00ff55a4e84c2
nrec77 <58744142+nrec77@users.noreply.github.com>
2021-10-05 15:10:48 -0500

    Update CreateToDo.js

--- src/CreateToDo.js
+++ src/CreateToDo.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function CreateToDo ({user}) {
+
+export default function CreateToDo ({user, dispatch}) {
+
+    const [ title, setTitle ] = useState('')
+    const [ description, setDescription ] = useState('')
+    const [ dateCreated, setDateCreated ] = useState('')
+    const [ complete, setComplete] = useState(false)
+    const [ dateCompleted, setDateCompleted ] = useState('')
+
+
+    function handleTitle (evt) { setTitle(evt.target.value) }
+
+    function handleDescription (evt) { setDescription(evt.target.value) }
+
+
+
+
      return (
-          <form onSubmit={e => e.preventDefault()}>
-             
+         
+        <form name="formm" onSubmit={e => {e.preventDefault(); dispatch({type: "CREATE_TODO", title, description, dateCreated, complete, dateCompleted});} }>
+            
              <div>Author: <b>{user}</b></div>
 
             <div>
                 <label htmlFor="create-title">Title : </label>
-                <input type="text" name="create-title" id="create-title" required="required" />
+                <input type="text" value={title} onChange={handleTitle} name="create-title"  id="create-title" required="required" />
+
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                 <label htmlFor="create=description">Description : </label>
-                <input type="text" name="create-description" id="create-description" />
+                <input type="text" value={description} onChange={handleDescription} name="create-description"  id="create-description" />
             </div>
-        
+
             <input type="submit" value="Create" />
-            
          </form>   
           )
  }
