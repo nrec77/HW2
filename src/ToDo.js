4af318b78a38afb313c6abfcce98c83418a7a65f
nrec77 <58744142+nrec77@users.noreply.github.com>
2021-10-24 00:44:28 -0500

    Add files via upload

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useResource } from 'react-request-hook'
 import { StateContext } from './Contexts'
 
@@ -17,6 +17,17 @@ export default function ToDo ({ title, description, dateCreated, complete, dateC
           data : { 'complete': !complete}
       }))
 
+      useEffect(() => {
+        if (todo_delete && todo_delete.isLoading === true) {
+            dispatch({type: 'DELETE_TODO', todoId: todoId})
+        }
+      }, [todo_delete])
+      
+      useEffect(() => {
+        if (todo_toggle && todo_toggle.isLoading === true) {
+            dispatch({type: 'TOGGLE_TODO', complete:!complete, todoId: todoId})        }
+      }, [todo_toggle])
+
      return (
        <div>
           <h3>{title}</h3>
@@ -25,11 +36,10 @@ export default function ToDo ({ title, description, dateCreated, complete, dateC
           <div>Date Created : {dateCreated} </div>
           <span>Completed</span>
 
-          <input type='checkbox' checked={complete} onClick={e => {toggleTodo(); dispatch({type: 'TOGGLE_TODO', complete:!complete, todoId: todoId})}}></input>
+          <input type='checkbox' checked={complete} onClick={e => {e.preventDefault(); toggleTodo()}}></input>
           {complete && <><i> Completed on : {new Date(dateCompleted).toLocaleDateString('en-us')}</i></>}
           <br/>
-          <button onClick={(e) => {deleteTodo(id); dispatch({type: 'DELETE_TODO', todoId: todoId})}}>Delete Post</button>
-          
+          <button onClick={(e) => {e.preventDefault(); deleteTodo(); }}>Delete Post</button>
           <br/><hr/>
      </div> 
  )
